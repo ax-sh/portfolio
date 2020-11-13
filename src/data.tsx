2459a3a28e6f6da4b48e4e5e24b4b21754eb550d
Axmin <>
2020-11-13 19:03:03 +0545

    Create data.tsx

---
+++ src/data.tsx
@@ -0,0 +1,20 @@
+export const data = {
+  links: [
+    'https://www.linkedin.com/in/axmin/',
+    'https://github.com/ax-sh',
+    'https://codesandbox.io/u/ax-sh',
+    'https://ax-sh.github.io/portfolio/axmin-shrestha-cv.pdf',
+  ],
+  languages:[
+      'javascript/typescript',
+      'nodejs',
+      'python',
+      'java',
+      'SQL'
+    ],
+    human_language:{
+        English:'Intermidiate',
+        Nepali:'Conversational',
+        Newari:'Understandable'
+    }
+};
