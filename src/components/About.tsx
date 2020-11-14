f1cc92d97439687e2705707bfb9207ab42c2c57d
Axmin <>
2020-11-14 16:58:16 +0545

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -93,13 +93,19 @@ export const About = () => (
 		{/* <h1>About (ME)</h1> */}
 	<AboutWrapper>
 		<div className="left">
-			<div>
-				This defines the ability for a flex item to grow if necessary. 
-				It accepts a unit less value that serves as a proportion. 
-				It dictates what amount of the available space inside the flex container the item should take up.
-
-				If all items have flex-grow set to 1, the remaining space in the container will be distributed equally to all children. 
-				If one of the children has a value of 2, the remaining space would take up twice as much space as the others (or it will try to, at least).
+			<div className='about-me'>
+			<h1>Axmin Shrestha</h1>
+			<h2>Full Stack Developer</h2>
+			<p>
+			
+			
+			This defines the ability for a flex item to grow if necessary. 
+			It accepts a unit less value that serves as a proportion. 
+			It dictates what amount of the available space inside the flex container the item should take up.
+			
+			If all items have flex-grow set to 1, the remaining space in the container will be distributed equally to all children. 
+			If one of the children has a value of 2, the remaining space would take up twice as much space as the others (or it will try to, at least).
+			</p>
 			</div>
 		</div>
 		<div className="center">
