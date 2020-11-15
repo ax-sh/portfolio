1143fb2a27318ae2af3f9591ad25676e829ca9c5
Axmin <>
2020-11-16 00:02:45 +0545

    add scrollbar

--- src/ui/Work.jsx
+++ src/ui/Work.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export const Work = () => {
   return (
-    <div id="work">
+    <div id="work" >
       <h1>WORK</h1>
-      <div className="work-wrapper">
+      <div className="work-wrapper scroll-custom">
 
         {[...Array(51).keys()].map(i => <div>
           <span>
