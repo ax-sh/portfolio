6877f8ab290925e31ff7f3f251671560ab06c915
Axmin <>
2020-11-16 00:02:34 +0545

    add email

--- src/ui/Contact.jsx
+++ src/ui/Contact.jsx
@@ -1,24 +1,34 @@
 import React from 'react';
 // import { ExternalLinks } from './App';
 
-const links = {
+const data = {
+  email:'example@example.com',
+  cv:"",
+  links:{
     Github:"",
     'Linked in':"",
     'Code Sandbox':"",
     Twitter:"",
     Website:"",
+    CV:""
   }
+}
   
-  export const ExternalLinks =()=>(
+export const ExternalLinks =()=>(
       <div className="links">
           <ul>
-              {Object.entries(links).map((k, v) => <li key={v}><a target="_blank" href={v}>{k}</a></li>)}
+              {Object.entries(data.links).map((k, v) => <li key={v}><a target="_blank" href={v}>{k}</a></li>)}
           </ul>
       </div>
-  ) 
+) 
 
 export const Contact = () => (
   <div id="contact">
     <ExternalLinks />
+    <h1 >
+      <a id="email" href={"mailto:"+data.email}>
+        {data.email}
+      </a>
+    </h1>
   </div>
 );
