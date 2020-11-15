6270f8a25ff9cd2ec1a906fa352a8ef01d264386
Axmin <>
2020-11-15 16:02:28 +0545

    Improve folder structure

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
-import './index.css';
+import App from './ui/App.jsx';
+import './css/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
