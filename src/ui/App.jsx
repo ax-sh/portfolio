5a1e135ac5ce0f5054ebf2d749bb72cc0e5a6307
Axmin <>
2020-11-15 16:03:56 +0545

    clean code

--- src/ui/App.jsx
+++ src/ui/App.jsx
@@ -1,21 +1,11 @@
 import React, { useState, useEffect } from 'react';
-// import logo from './logo.svg';
 import '../css/App.scss';
 
 function App() {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
   return (
     <div className="App">
       <header className="App-header">
         HI
-
       </header>
     </div>
   );
