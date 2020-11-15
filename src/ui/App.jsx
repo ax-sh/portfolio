793deff3e600e05e7d71ac484e607733e62b4bbd
Axmin <>
2020-11-15 16:33:21 +0545

    wip

--- src/ui/App.jsx
+++ src/ui/App.jsx
@@ -1,12 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import '../css/App.scss';
 
+const links = {
+  github:"",
+  linkedin:"",
+  codesandbox:"",
+  twitter:"",
+  github:"",
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        HI
-      </header>
+      {/* <header className="App-header">
+        nav
+      </header> */}
+      {/* <main> */}
+        <div className="contact">
+          <div className="links">
+            <ul>
+              {Object.entries(links).map((k, v)=><li><a href={v}>{k}</a></li>)}
+            </ul>
+          </div>
+        </div>
+      {/* </main> */}
     </div>
   );
 }
