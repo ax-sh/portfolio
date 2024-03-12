832b513322666dc4f9498864c866ca31548ccb63
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-03-12 14:56:46 +0545

    chore: comment out git flow release commands in .release-it.cjs

--- .release-it.cjs
+++ .release-it.cjs
@@ -36,7 +36,7 @@ module.exports = {
 			"git update-index -q --refresh",
 		],
 		"before:beforeBump": [
-			"git flow release start v${version}",
+			// "git flow release start v${version}",
 			"pnpm pretty-quick",
 			'git commit  --allow-empty -am "chore: prettify before release"',
 			"echo \uD83D\uDC4A before:bump version=v${version} latestVersion=v${latestVersion}",
@@ -45,10 +45,10 @@ module.exports = {
 			"git push origin --tags",
 			"echo \uD83D\uDE4C Successfully released ${name} v${version} to ${repo.repository}.",
 			"git push -u origin HEAD",
-			"git flow release finish --notag",
-			"git push origin refs/heads/master:master",
+			// "git flow release finish --notag",
+			// "git push origin refs/heads/master:master",
 			"echo Successfully released ${name} v${version} to ${repo.repository}.",
 		],
-		// "after:git:release": "gh pr create --fill -B master",
+		"after:git:release": "gh pr create --fill -B master",
 	},
 };
