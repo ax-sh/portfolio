31d035cb9b490c4f4b6fa6d8cdcbd06fe2ab97c8
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-03-12 15:11:15 +0545

    style: Update formatting in app.tsx for better readability

--- src/app.tsx
+++ src/app.tsx
@@ -2,21 +2,31 @@ import React from "react";
 import { useRoutes } from "react-router-dom";
 import clsx from "clsx";
 import routes from "~react-pages";
+import { log } from "console";
 
 export function Loader() {
-	return <h5 className="prose text-6xl">Loading...</h5>;
+	return <h5 className="animate-ping prose text-6xl">LOADING...</h5>;
 }
 
 export function ScreenLoader() {
 	return (
-		<section className={clsx("animate-ping h-screen", "grid place-content-center")}>
+		<section
+			className={clsx(
+				"w-dvw h-dvh overflow-hidden",
+				"grid place-content-center",
+			)}
+		>
 			<Loader />
 		</section>
 	);
 }
 
 function App() {
-	return <React.Suspense fallback={<ScreenLoader />}>{useRoutes(routes)}</React.Suspense>;
+	return (
+		<React.Suspense fallback={<ScreenLoader />}>
+			{useRoutes(routes)}
+		</React.Suspense>
+	);
 }
 
 export default App;
